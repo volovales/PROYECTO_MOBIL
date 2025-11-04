9a29bd1c728cf6be167719920a6cf6c0ff0512a0
volovales <124051396@upq.edu.mx>
2025-11-04 00:34:36 -0600

    proyecto funcional
    Sí trabajo para que el proyecto funcionara adecuadamente haciendo cambios y añadiendo las carpetas y funciones que hay nuestros compañeros trabajaron

--- proyecto/App.js
+++ proyecto/App.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 // Importa tus pantallas
 import Menu from './screens/Menu';
+import InicioSesion from './screens/InicioSesion';
+import Registro from './screens/Registro';
 import HomeScreen from './screens/HomeScreen';
-import TransaccionesScreen from './screens/TransaccionesScreen';
+import TransaccionesScreen from './screens/TransacciScreen';
+import PresupuestoScreen from './screens/PresupuestoScreen';
 import NotifiScreen from './screens/NotifiScreen';
 
 const Stack = createNativeStackNavigator();
@@ -14,8 +18,11 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Menu">
         <Stack.Screen name="Menu" component={Menu} />
+        <Stack.Screen name="InicioSesion" component={InicioSesion} />
+        <Stack.Screen name="Registro" component={Registro} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Transacciones" component={TransaccionesScreen} />
+        <Stack.Screen name="Presupuesto" component={PresupuestoScreen} />
         <Stack.Screen name="Notificaciones" component={NotifiScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- proyecto/screens/Menu.js
+++ proyecto/screens/Menu.js
@@ -4,9 +4,13 @@ export default function Menu({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Menú de Pantallas</Text>
-      <Button title="Ir a Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Ir a Transacciones" onPress={() => navigation.navigate('Transacciones')} />
-      <Button title="Ir a Notificaciones" onPress={() => navigation.navigate('Notificaciones')} />
+
+      <Button title="Inicio de Sesión" onPress={() => navigation.navigate('InicioSesion')} />
+      <Button title="Registro" onPress={() => navigation.navigate('Registro')} />
+      <Button title="Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Transacciones" onPress={() => navigation.navigate('Transacciones')} />
+      <Button title="Presupuesto" onPress={() => navigation.navigate('Presupuesto')} />
+      <Button title="Notificaciones" onPress={() => navigation.navigate('Notificaciones')} />
     </View>
   );
 }
