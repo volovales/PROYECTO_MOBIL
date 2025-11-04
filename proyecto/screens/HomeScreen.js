06c17a63fe60476696015435db866d652c8ae6b7
LeonardoValdelamarMartinez <124048844@upq.edu.mx>
2025-11-04 10:58:57 -0600

    Update HomeScreen
    Se le agregaron cosas al home screen

--- proyecto/screens/HomeScreen.js
+++ proyecto/screens/HomeScreen.js
@@ -1,32 +1,47 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-
 export default function HomeScreen({ navigation }) {
   return (
+    
     <View style={styles.container}>
+      
       <Text style={styles.title}>Bienvenido</Text>
+
+      <View style={styles.container2} >
+      <Button color = 'black' title="Ingresos" onPress={() => navigation.navigate('Ingresos')} />
+      <Button color = 'grey' title="Gastos" onPress={() => navigation.navigate('Gastos')} />
+      </View>
       <Text style={styles.subtitulo}>Sus finanzas son:</Text>
 
       <View style={styles.card}>
         <Text style={styles.cardTitle}>Saldo actual:</Text>
         <Text style={styles.balance}>$0.00</Text>
       </View>
-
-      <Button title="Ver transacciones" onPress={() => navigation.navigate('Transacciones')} />
-      <Button title="Presupuesto" onPress={() => navigation.navigate('Presupuestos')} />
-      <Button title="Ver gráficas" onPress={() => navigation.navigate('Graficas')} />
-      <Button title="Ver perfil" onPress={() => navigation.navigate('Perfil')} />
+      <Button color = 'grey' title="Ver transacciones" onPress={() => navigation.navigate('Transacciones')} />
+      <Button color = 'grey' title="Presupuesto" onPress={() => navigation.navigate('Presupuestos')} />
+      <Button color = 'grey' title="Ver gráficas" onPress={() => navigation.navigate('Graficas')} />
+      <Button color = 'grey' title="Ver perfil" onPress={() => navigation.navigate('Perfil')} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background:{
+
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+  },
+  container2: {
+    flexDirection: "row",
+    justifyContent: "space-between", 
+    gap: 20,
+    padding: 20,
+    fontSize: 50,
   },
   title: {
     fontSize: 28,
@@ -54,11 +69,15 @@ const styles = StyleSheet.create({
     fontSize: 19,
     fontWeight: 'bold',
     marginBottom: 5,
+    textAlign: 'center',
   },
   balance: {
     fontSize: 22,
     color: '#009688',
     fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  boton: {
+    color: 'grey',
   },
 });
- 
