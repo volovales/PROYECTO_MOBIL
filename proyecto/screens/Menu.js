d16195da76ff9671ebf619e57117fa11529de6f6
LeonardoValdelamarMartinez <124048844@upq.edu.mx>
2025-11-04 11:08:58 -0600

    Update Menu
    Se añadieron botones para poder navegar entre las diferentes diapositivas

--- proyecto/screens/Menu.js
+++ proyecto/screens/Menu.js
@@ -1,31 +1,70 @@
+import React, { useState } from 'react';
 import { Text, StyleSheet, View, Button } from 'react-native';
 
-export default function Menu({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Menú de Pantallas</Text>
+import InicioSesion from './InicioSesion';
+import Registro from './Registro';
+import Actividad from './Actividad';
+import HomeScreen from './HomeScreen';
+import NotifiScreen from './NotiScreen';
+import TransaccionesScreen from './Transacciones';
+import PresupuestoScreen from './PresupuestoScreen';
 
-      <Button title="Inicio de Sesión" onPress={() => navigation.navigate('InicioSesion')} />
-      <Button title="Registro" onPress={() => navigation.navigate('Registro')} />
-      <Button title="Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Transacciones" onPress={() => navigation.navigate('Transacciones')} />
-      <Button title="Presupuesto" onPress={() => navigation.navigate('Presupuesto')} />
-      <Button title="Notificaciones" onPress={() => navigation.navigate('Notificaciones')} />
-    </View>
-  );
+
+export default function MenuScreen() {
+  const [screen, setScreen] = useState('menu');
+
+  switch (screen) 
+  {
+
+    case 'InicioSesion':
+      return <InicioSesion/>;
+    
+    case 'Registro':
+      return <Registro/>;
+
+    case 'Actividad':
+      return <Actividad/>;
+    
+      case 'HomeScreen':
+      return <HomeScreen/>;
+    
+      case 'NotifiScreen':
+      return <NotifiScreen/>;
+    
+      case 'Transacciones':
+      return <TransaccionesScreen/>;
+    
+      case 'Presupuesto':
+      return <PresupuestoScreen/>;
+
+    default:
+      return (
+        <View style = {styles.container}>
+          <Text style ={styles.texto} >Menu</ Text>
+          <Button color = 'grey' onPress={()=>setScreen('InicioSesion')} title='Practica Inicio Sesion'></Button>
+          <Button color = 'grey' onPress={()=>setScreen('Registro')} title='Practica Registro'></Button>
+            <Button color = 'grey' onPress={()=>setScreen('Actividad')} title='Practica Actividad'></Button>
+            <Button color = 'grey' onPress={()=>setScreen('HomeScreen')} title='Practica HomeScreen'></Button>
+            <Button color = 'grey' onPress={()=>setScreen('NotifiScreen')} title='Practica Notificaciones'></Button>
+            <Button color = 'grey' onPress={()=>setScreen('Transacciones')} title='Practica Transacciones'></Button>
+            <Button color = 'grey' onPress={()=>setScreen('Presupuesto')} title='Practica Presupuesto'></Button>
+          </View>
+      );
+    }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#aeaaaaff',
     alignItems: 'center',
-    backgroundColor: 'white',
-    gap: 10,
-  },
-  title: {
-    fontSize: 20,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+},
+ texto: {
+    color: 'black',
+    fontSize: 60,
     fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
+    fontStyle: 'italic',
+  }
+}
+);
