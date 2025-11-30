37d29b84427044dee0032cbd3c7044fb7968742c
Emiliano-SB <124051706@upq.edu.mx>
2025-11-30 17:51:14 -0600

    Update PresController.js

--- proyecto/controllers/PresController.js
+++ proyecto/controllers/PresController.js
@@ -1,19 +1,34 @@
 import DatabaseService from "../database/DatabaseService";
 import { Presupuesto } from "../models/Presupuesto";
 
+//controlador para manejar la logica del presupuesto
 export class PresController {
 
+    //crear un nuevo presupuesto
     async crear(categoria, limite) {
-        if (!categoria) throw new Error("La categoría no puede estar vacía");
-        if (limite <= 0) throw new Error("El límite debe ser mayor a 0");
 
-        await DatabaseService.agregarPresupuesto(categoria, limite);
+        const nombreFinal =
+            categoria && categoria.trim().length > 0
+                ? categoria.trim()
+                : "Sin categoría";
+        
+        //valida si los datos son correctos tal y como se pide 
+        Presupuesto.validar(nombreFinal, limite);
+
+        await DatabaseService.agregarPresupuesto(nombreFinal, limite);
     }
 
+    //Obtiene todos los presupuestos del usuario
     async obtenerTodos() {
         const data = await DatabaseService.obtenerPresupuestos();
-        return data.map(p => new Presupuesto(
-            p.id, p.categoria, p.limite, p.gastado
-        ));
+        return data.map(
+            (p) =>
+                new Presupuesto(
+                    p.id,
+                    p.categoria,
+                    p.limite,
+                    p.gastado
+                )
+        );
     }
 }
