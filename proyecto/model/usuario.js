2e45b646e8530f8d08a6928ba50cf059a6ebca15
LeonardoValdelamarMartinez <124048844@upq.edu.mx>
2025-11-30 21:25:37 -0600

    Update usuario

--- proyecto/model/usuario.js
+++ proyecto/model/usuario.js
@@ -1,18 +1,21 @@
 export class Usuario {
-    constructor (id, nombre, fechaCreacion) {
-        this.id = id;
-        this.nombre = nombre; 
-        this.fechaCreacion = fechaCreacion || new Date().toISOString();
+
+    constructor(id, nombre, fechaCreacion) {
+        this.id = id;                                  // Asigna el ID del usuario
+        this.nombre = nombre;                          // Guarda el nombre del usuario
+        this.fechaCreacion = fechaCreacion ||          // Si viene fecha de BD, úsala;
+            new Date().toISOString();                  // si no, genera fecha actual automáticamente
     }
 
-    //validaciones del modelo 
     static validar(nombre) {
-        if (!nombre || nombre.trim().length== 0) {
-            throw new Error ('El nombre no puede estar vacio ');
+        if (!nombre || nombre.trim().length == 0) {    // Verifica que el nombre NO esté vacío
+            throw new Error('El nombre no puede estar vacio ');
         }
-        if (nombre.lenght > 50) {
-            throw new Error ('El nombre no puede tener más de 50 caracteres');
+
+        if (nombre.length > 50) {                      // Verifica que el nombre no exceda 50 caracteres
+            throw new Error('El nombre no puede tener más de 50 caracteres');
         }
-        return true;
+
+        return true;                                   // Si pasa las validaciones, regresa true
     }
 }
